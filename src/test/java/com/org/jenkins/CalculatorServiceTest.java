2367377249b92ea74cd537efab2652e930153a5b
unknown <rafcasto@gmail.com>
2021-03-29 11:59:04 +1300

    adding caculator test

--- src/test/java/com/org/jenkins/CalculatorServiceTest.java
+++ src/test/java/com/org/jenkins/CalculatorServiceTest.java
@@ -1,4 +1,23 @@
+package com.org.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
 class CalculatorServiceTest {
-  
+    @Autowired
+    private CalculatorService calculatorService;
+
+    @Test
+    void sumPositives() {
+        assertEquals(5,calculatorService.sum(2,3));
+    }
+
+    @Test
+    void sumNegatives() {
+        assertEquals(-5,calculatorService.sum(-2,-3));
+    }
+
 }
